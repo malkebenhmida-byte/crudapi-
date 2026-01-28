e27fa247dc90c196ec61b087e2907314ba4292f7
malek ben hmida <malkebenhmida@gmail.com>
2026-01-28 12:45:50 +0100

    backend

--- src/main/java/com/example/crudapi/entity/User.java
+++ src/main/java/com/example/crudapi/entity/User.java
@@ -15,6 +15,9 @@ public class User {
     @Enumerated(EnumType.STRING)
     private Role role ;
 
+    public User() {
+    }
+
     public User(String username, String email, String password, Role role) {
         this.username = username;
         this.email = email;
